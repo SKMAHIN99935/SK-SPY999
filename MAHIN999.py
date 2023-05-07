712924b5e76cd7d0a68edb2c95f181ca6468e058
SK MH99935 <104576528+SKMAHIN99935@users.noreply.github.com>
2023-05-07 20:09:17 +0600

    Update MAHIN999.py

--- MAHIN999.py
+++ MAHIN999.py
@@ -2,7 +2,7 @@ import os
 import platform
 os.system('clear')
 os.system("git pull")
-b = platform.architecture()[0
+ b = platform.architecture()[0
 if b == '64bit':
 __import__("MAHIN999").Spy()
 elif b == '32bit':

f92e7aa81b968966b12ef3f1c3058217a4e8cc74
SK MH99935 <104576528+SKMAHIN99935@users.noreply.github.com>
2023-05-07 20:03:39 +0600

    Update MAHIN999.py

--- MAHIN999.py
+++ MAHIN999.py
@@ -4,7 +4,7 @@ os.system('clear')
 os.system("git pull")
 b = platform.architecture()[0
 if b == '64bit':
-__import__("SPYVIP").Spy()
+__import__("MAHIN999").Spy()
 elif b == '32bit':
  print(" 32 BIT WILL AVAILABLE SOON ")                            
 
